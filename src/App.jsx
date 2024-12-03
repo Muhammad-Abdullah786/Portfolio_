7f0680d4a6873f70d603e3f373e3b48be1edb7f0
Muhammad Abdullah <abdulx125@gmail.com>
2024-12-03 18:44:30 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,24 @@
-import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import {
-  Hero,
-  Navbar,
-  About,
-  Tech,
-  Experience,
-  Works,
-  Feedbacks,
-  Contact,
-  EarthCanvas,
-  BallCanvas,
-  ComputersCanvas,
-  StarsCanvas,
-} from "./components/index";
-function App() {
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+
+const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0  bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-ce">
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
           <Hero />
         </div>
-
         <About />
         <Experience />
         <Tech />
         <Works />
         <Feedbacks />
-        <div className="relative z-0">
+        <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
-        <EarthCanvas />
-        <BallCanvas />
-        <ComputersCanvas />
       </div>
     </BrowserRouter>
   );
